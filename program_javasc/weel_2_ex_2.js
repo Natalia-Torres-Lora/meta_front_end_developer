b473b9e959f83fac56945a3f68597ab21bf487df
Natalia Torres <torresloranatalia@gmail.com>
2023-02-06 21:55:04 -0500

    Added 2 completed assignments

---
+++ program_javasc/weel_2_ex_2.js
@@ -0,0 +1,32 @@
+// Exercise: Creating arrays and objects
+// Create a new empty array literal and assign it to the variable clothes.
+// Add 5 of your favorite items of clothing as strings using the push() method.
+// Remove the fifth piece of clothing from the array using the pop() method.
+// Add a new piece of clothing using the push() method.
+// Use console.log to show the third item from the clothes array in the console.
+// Create a new empty object literal and assign it to the variable favCar.
+// Using the dot notation, assign a color property to the favCar object and give it a string value with the color of your choice.
+// Using the dot notation, assign a covertible property to the favCar object and give it a boolean value of your choice.
+// Use the console to log the entire favCar object.
+
+var clothes = [];
+
+clothes.push('pants', 'dress', 'heals', 'crop');
+
+clothes.pop()
+
+clothes.push('ring')
+
+console.log(clothes[2])
+
+var favCar = {};
+
+favCar.color = 'blue';
+
+favCar.convertible = true;
+
+console.log(favCar)
+
+
+
+
